0d5596ae2b28e743b2fb5130b7ac4489807ba0b5
sunminigo <sunminigo@gmail.com>
2021-01-31 23:38:11 +0900

    view 1

--- dist/assets/script/common.js
+++ dist/assets/script/common.js
@@ -6,22 +6,18 @@ $(document).ready(function(){
 	$('.gnb').each(function () {
 		var ref = $(this).children('button').attr('ref');
 		var href = $(this).find('a').attr('href');
-		// console.log('urlName:', urlName);
-		// console.log('ref:', ref);
 
 		if (urlName.indexOf(ref) != -1) {
 			$(this).addClass('active');
 			$(this).find('.lnb_wrap').addClass('active')
-			if ($(this).children().hasClass('lnb_wrap')) {
-				$(this).parent().addClass('active')
-			}
 		}
 
 		$(this).on('click', function() {
 			if ($(this).children().hasClass('lnb_wrap')) {
 				console.log('있어')
-				$('.gnb_wrap').addClass('active');
+				$(this).addClass('active');
 				$(this).find('.lnb_wrap').addClass('active');
+				$('.gnb').not($(this)).removeClass('active').find('.lnb_wrap').removeClass('active');
 				return true;
 			} else {
 				console.log('없어')
@@ -40,7 +36,6 @@ $(document).ready(function(){
 			$(this).addClass('active');
 		}
 	});
-
 	$('.btn_back').on('click', function(){
 		$('.gnb_wrap').removeClass('active');
 		$('.lnb_wrap').removeClass('active');
@@ -48,55 +43,23 @@ $(document).ready(function(){
 		return false;
 	});
 
-/*
-		var url = document.location.pathname
-		var gnb = document.querySelectorAll('.gnb');
-		var btn_back = document.getElementsByClassName('btn_back');
-
-		for (var i = 0; i < gnb.length; i++) {
-			var refName = gnb[i].children[0].attributes[1].value;
-
-			if (url.includes(refName)) {
-				gnb[i].className += ' active';
-				console.log('11111111', gnb[i].children[1]);
-
-				if (gnb[i].children[1]) {
-					console.log('2222222222222')
-					gnb[i].parentNode.className += ' active';
-					gnb[i].children[1].className += ' active';
-
-					$('.btn_back').click(function() {
-						console.log('dddddddddddddd');
-						$('.gnb_wrap').removeClass('active');
-						console.log($(this).parents('.lnb_wrap'))
-						$(this).parents('.lnb_wrap').removeClass('active');
-					});
-				}
-			} else {
-				gnb[i].className += '';
-				if (gnb[i].children[1]) {
-					gnb[i].parentNode.className += '';
-					gnb[i].children[1].className += '';
-				}
-			}
-		}
-
-	*/
-
-/*
-	$('.gnb > button').click(function() {
-		console.log($(this))
-		if ($(this).next($('lnb_wrap'))) {
-			console.log('있어')
-				$(this).addClass('active');
-				$(this).find('.lnb_wrap').addClass('active');
-				$('.gnb').not($(this)).removeClass('active').find('.lnb_wrap').removeClass('active');
-		} else {
-			console.log('없어');
-			$(this).parents('.gnb_wrap').removeClass('active');
-		}
+	//popup open
+	$('[data-popup-open]').on('click', function(e)  {
+		var targeted_popup_class = $(this).attr('data-popup-open');
+		$('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+		e.preventDefault();
 	});
-*/
+	//popup close
+	$('[data-popup-close]').on('click', function(e)  {
+		var targeted_popup_class = $(this).attr('data-popup-close');
+		$('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+		e.preventDefault();
+	});
+
+	// 텍스트 사이즈
+	$(".control_text").bind("click", function () {
+		$('body').toggleClass('controlFontSize');
+	});
 
 	//slider
 	$('#visual_slider').slick({
@@ -116,24 +79,23 @@ $(document).ready(function(){
 		verticalSwiping: true
 	});
 
-	// 대관현황 전체보기 팝업
-	$('.btn_schedule').on('click', function() {
-		$('.popup_wrap').addClass('active');
-		return false;
-	});
-	$('.btn_close').on('click', function() {
-		$('.popup_wrap').removeClass('active');
-		return false;
+	$(".tabs").slick({
+		asNavFor: '.tab_content',
+		slidesToShow: 6,
+		autoplay: false,
+		arrows: false,
 	});
 
-	// 텍스트 사이즈
-	$(".control_text").bind("click", function () {
-		$('body').toggleClass('controlFontSize');
+	$(".tab_content").slick({
+		asNavFor: '.tabs',
+		slidesToShow: 1,
+		autoplay: false,
+		arrows: false,
 	});
-
 });
 
 var nowZoom = 100;
+
 function zoomOut() {
 	nowZoom = nowZoom - 10;
 	if(nowZoom <= 100) nowZoom = 100;
@@ -147,13 +109,10 @@ function zoomIn() {
 function zooms() {
 	var wrapper = document.getElementById('zoom');
 			wrapper.style.width = nowZoom + "%";
-	var wrapper = document.getElementById('zoom');
 			wrapper.style.height = nowZoom + "%";
-	var wrapper = document.getElementById('zoom');
 			wrapper.style.zoom = nowZoom + "%";
 
 	if(nowZoom === 200) {
 		alert("더 이상 확대할 수 없습니다.");
 	}
 }
-
